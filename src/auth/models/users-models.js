e67d909055fe56986536a2a6e0fe132066332822
ReeceRenninger <renningerreece@gmail.com>
2023-05-22 13:37:28 -0700

    modularization

--- src/auth/models/users-models.js
+++ src/auth/models/users-models.js
@@ -3,7 +3,11 @@
 const bcrypt = require('bcrypt');
 const base64 = require('base-64');
 const { Sequelize, DataTypes } = require('sequelize');
+const { app } = require('../server');
+const express = require('express');
 
+//Process FORM input and put he data on req.body
+app.use(express.urlencoded({ extended: true}));
 
 //create the sequelize model
 const Users = sequelize.define('User', {
@@ -17,3 +21,11 @@ const Users = sequelize.define('User', {
   },
 });
 
+
+app.post('/signup', async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+});
